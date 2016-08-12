7cb26330f75d42e4638cda3d923287cc606104b6
CN Williams <cw14910@bristol.ac.uk>
2016-08-12 11:02:23 +0100

    added js and css with links in index.html

---
+++ js/vis3d.js
@@ -0,0 +1,39 @@
+// the csv is literally just an ascii file... https://github.com/plotly/datasets/blob/master/api_docs/mt_bruno_elevation.csv
+// e.g.
+// 1 1 2 2 
+// 1 1 2 2 
+// 1 1 2 2 
+// 1 1 2 2 
+
+
+//Plotly.d3.csv('https://github.com/Chris35Wills/bed2013_3dvis/blob/master/data/aoi3.csv', function(err, rows){
+Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv', function(err, rows){
+function unpack(rows, key) {
+  return rows.map(function(row) { return row[key]; });
+}
+  
+var z_data=[ ]
+for(i=0;i<24;i++)
+{
+  z_data.push(unpack(rows,i));
+}
+
+var data = [{
+           z: z_data,
+           type: 'surface'
+        }];
+  
+var layout = {
+  title: 'Mt Bruno Elevation',
+  autosize: false,
+  width: 800,
+  height: 800,
+  margin: {
+    l: 65,
+    r: 50,
+    b: 65,
+    t: 90,
+  }
+};
+Plotly.newPlot('myDiv', data, layout);
+});
