1c18337ab6f4eacf66360ec925e12c3c6bc44df4
Chris <chris35williams@btinternet.com>
2016-08-26 17:56:11 +0100

    js2 all in braces

--- js/vis3d2.js
+++ js/vis3d2.js
@@ -1,14 +1,39 @@
 
-Plotly.d3.csv('https://raw.githubusercontent.com/Chris35Wills/bed2013_3dvis/gh-pages/data/dem2012_5km.csv', function(err, rows){
+//Plotly.d3.csv('https://raw.githubusercontent.com/Chris35Wills/bed2013_3dvis/gh-pages/data/dem2012_5km.csv', function(err, rows){
+//
+//function unpack(rows, key) {
+//  return rows.map(function(row) { return row[key]; });
+//}
+//  
+//var z_data=[ ]
+////for(i=0;i<24;i++) // as test.csv
+////for(i=0;i<599;i++) // for dem2015_5km --> need to remove hardwiring
+//for(i=0;i<rows.length;i++) // for dem2015_5km --> need to remove hardwiring
+//{
+//  z_data.push(unpack(rows,i));
+//}
+//
+//var data = [{
+//           z: z_data,
+//           type: 'surface'
+//        }];
+//
+//}
+//
+//Plotly.newPlot('mydiv', data, {title:'Bedmap 2013b'});
+//var plotDiv = document.getElementById('mydiv');
+//var plotData = plotDiv.data;
 
+/////////////////////////////////
+
+//Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv', function(err, rows){
+Plotly.d3.csv('https://raw.githubusercontent.com/Chris35Wills/bed2013_3dvis/gh-pages/data/dem2012_5km.csv', function(err, rows){
 function unpack(rows, key) {
   return rows.map(function(row) { return row[key]; });
 }
   
 var z_data=[ ]
-//for(i=0;i<24;i++) // as test.csv
-//for(i=0;i<599;i++) // for dem2015_5km --> need to remove hardwiring
-for(i=0;i<rows.length;i++) // for dem2015_5km --> need to remove hardwiring
+for(i=0;i<599;i++) // for dem2015_5km --> need to remove hardwiring
 {
   z_data.push(unpack(rows,i));
 }
@@ -18,9 +43,28 @@ var data = [{
            type: 'surface'
         }];
 
-}
+var axis = {
+    xaxis: {
+        title: 'x Axis',
+        titlefont: {
+          family: 'Courier New, monospace',
+          size: 18,
+          color: '#7f7f7f'
+        }
+    },
+    yaxis: {
+        title: 'y Axis',
+        titlefont: {
+          family: 'Courier New, monospace',
+          size: 18,
+          color: '#7f7f7f'
+        }
+    }
+    }
 
-Plotly.newPlot('mydiv', data, {title:'Bedmap 2013b'});
+Plotly.newPlot('mydiv', data, axis, {title:'Bedmap 2013b'});
 var plotDiv = document.getElementById('mydiv');
 var plotData = plotDiv.data;
+
+});
 
