c9340d6e56f4ff026d945dfb7c4a7452a4dad217
Chris <chris35williams@btinternet.com>
2016-08-26 18:28:14 +0100

    remove no data vals

--- js/vis3d2.js
+++ js/vis3d2.js
@@ -1,31 +1,3 @@
-
-//Plotly.d3.csv('https://raw.githubusercontent.com/Chris35Wills/bed2013_3dvis/gh-pages/data/dem2012_5km.csv', function(err, rows){
-//
-//function unpack(rows, key) {
-//  return rows.map(function(row) { return row[key]; });
-//}
-//  
-//var z_data=[ ]
-////for(i=0;i<24;i++) // as test.csv
-////for(i=0;i<599;i++) // for dem2015_5km --> need to remove hardwiring
-//for(i=0;i<rows.length;i++) // for dem2015_5km --> need to remove hardwiring
-//{
-//  z_data.push(unpack(rows,i));
-//}
-//
-//var data = [{
-//           z: z_data,
-//           type: 'surface'
-//        }];
-//
-//}
-//
-//Plotly.newPlot('mydiv', data, {title:'Bedmap 2013b'});
-//var plotDiv = document.getElementById('mydiv');
-//var plotData = plotDiv.data;
-
-/////////////////////////////////
-
 //Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv', function(err, rows){
 Plotly.d3.csv('https://raw.githubusercontent.com/Chris35Wills/bed2013_3dvis/gh-pages/data/dem2012_5km.csv', function(err, rows){
 function unpack(rows, key) {
@@ -35,7 +7,17 @@ function unpack(rows, key) {
 var z_data=[ ]
 for(i=0;i<rows.length;i++) // for dem2015_5km --> need to remove hardwiring
 {
-  z_data.push(unpack(rows,i));
+  z_data.push(unpack(rows,i)); // push appends things
+}
+
+//remove no data values from array
+no_data=-9999.0
+
+for(i=0;i<z_data.length;i++) // for dem2015_5km --> need to remove hardwiring
+{
+  if(z_data[i]==no_data){
+  	z_data[i]=' '
+  }
 }
 
 var data = [{
@@ -57,30 +39,6 @@ var layout = {
 };
 
 Plotly.newPlot('mydiv', data, layout);
-
-//var plotDiv = document.getElementById('mydiv');
-//var plotData = plotDiv.data;
-//
-//var axis = {
-//    xaxis: {
-//        title: 'x Axis',
-//        titlefont: {
-//          family: 'Courier New, monospace',
-//          size: 18,
-//          color: '#7f7f7f'
-//        }
-//    },
-//    yaxis: {
-//        title: 'y Axis',
-//        titlefont: {
-//          family: 'Courier New, monospace',
-//          size: 18,
-//          color: '#7f7f7f'
-//        }
-//    }
-//  }
-//
-//Plotly.restyle('mydiv', axis)
 
 });
 
